9527d5d83a1a3431425b74a60a010a9f73b9ec8f
Steve Kirkegard <Steve.Kirkegard@sunshinecoast.qld.gov.au>
2016-10-26 11:39:46 +1000

    leaflet and gis services example

---
+++ leaflet-gisservices-example/data.js
@@ -0,0 +1,274 @@
+/*  
+Data on Patrolled Beaches. Retrieved from Sunshine Coast Council GIS Services on 26/10/2016.
+For reference, the following query was run, which returned the data placed in this file:
+http://gisservices.scc.qld.gov.au/arcgis/rest/services/Society/Society_SCRC/MapServer/6/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson
+*/
+var data = 
+        {
+	"displayFieldName" : "Description",
+	"fieldAliases" : {
+		"Description" : "Description"
+	},
+	"geometryType" : "esriGeometryPoint",
+	"spatialReference" : {
+		"wkid" : 4326,
+		"latestWkid" : 4326
+	},
+	"fields" : [{
+			"name" : "Description",
+			"type" : "esriFieldTypeString",
+			"alias" : "Description",
+			"length" : 250
+		}
+	],
+	"features" : [{
+			"attributes" : {
+				"Description" : "Kings Beach"
+			},
+			"geometry" : {
+				"x" : 153.14218743021868,
+				"y" : -26.804163083302324
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Dicky Beach"
+			},
+			"geometry" : {
+				"x" : 153.13967096613851,
+				"y" : -26.782920120369777
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Wurtulla Beach"
+			},
+			"geometry" : {
+				"x" : 153.13604467125407,
+				"y" : -26.748746023466889
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Kawana Beach"
+			},
+			"geometry" : {
+				"x" : 153.13585846756365,
+				"y" : -26.703751864584646
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Mooloolaba Beach (Catamarans)"
+			},
+			"geometry" : {
+				"x" : 153.13179813817499,
+				"y" : -26.683326929829526
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Mooloolaba Beach (The Spit) "
+			},
+			"geometry" : {
+				"x" : 153.12727666672004,
+				"y" : -26.683438573100307
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Mooloolaba Beach"
+			},
+			"geometry" : {
+				"x" : 153.12160063015631,
+				"y" : -26.680224588700408
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Alexandra Headland beach"
+			},
+			"geometry" : {
+				"x" : 153.10858813621053,
+				"y" : -26.669835697246189
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Maroochydore Beach"
+			},
+			"geometry" : {
+				"x" : 153.10413123206493,
+				"y" : -26.656042625259463
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Twin Waters Beach"
+			},
+			"geometry" : {
+				"x" : 153.10165715858471,
+				"y" : -26.630972408348157
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Mudjimba Beach"
+			},
+			"geometry" : {
+				"x" : 153.10187280276196,
+				"y" : -26.614893255043089
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Discovery Beach"
+			},
+			"geometry" : {
+				"x" : 153.09834787197755,
+				"y" : -26.602142151746627
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Marcoola Beach"
+			},
+			"geometry" : {
+				"x" : 153.09783449467415,
+				"y" : -26.583131280878412
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Boardwalk Beach"
+			},
+			"geometry" : {
+				"x" : 153.09871793475546,
+				"y" : -26.569840662085589
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Hyatt Beach"
+			},
+			"geometry" : {
+				"x" : 153.09984028987148,
+				"y" : -26.560532774613399
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Coolum Beach"
+			},
+			"geometry" : {
+				"x" : 153.09311990519606,
+				"y" : -26.531702561045332
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Coolum (North) Beach"
+			},
+			"geometry" : {
+				"x" : 153.09285258543213,
+				"y" : -26.528842087913183
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Golden Beach"
+			},
+			"geometry" : {
+				"x" : 153.12258639553505,
+				"y" : -26.811460969543326
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Bulcock Beach"
+			},
+			"geometry" : {
+				"x" : 153.1317529753741,
+				"y" : -26.807187465458867
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Bulcock Beach"
+			},
+			"geometry" : {
+				"x" : 153.13159578187751,
+				"y" : -26.80708084808472
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Kings Beach"
+			},
+			"geometry" : {
+				"x" : 153.14228045076632,
+				"y" : -26.804037108558102
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Dicky Beach"
+			},
+			"geometry" : {
+				"x" : 153.13959900030449,
+				"y" : -26.782784758525885
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Currimundi Beach"
+			},
+			"geometry" : {
+				"x" : 153.13681634564273,
+				"y" : -26.766323726242288
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Kawana Beach"
+			},
+			"geometry" : {
+				"x" : 153.13587695129902,
+				"y" : -26.703611635972031
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Mooloolaba Beach"
+			},
+			"geometry" : {
+				"x" : 153.12150741557255,
+				"y" : -26.680108350211576
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Alexandra Headland beach"
+			},
+			"geometry" : {
+				"x" : 153.10849924072659,
+				"y" : -26.669673276022706
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Maroochydore Beach"
+			},
+			"geometry" : {
+				"x" : 153.10410723626708,
+				"y" : -26.655916758562601
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Mudjimba Beach"
+			},
+			"geometry" : {
+				"x" : 153.10180629688818,
+				"y" : -26.614753089874679
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Marcoola Beach"
+			},
+			"geometry" : {
+				"x" : 153.09783703331314,
+				"y" : -26.582948080597625
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Coolum Beach"
+			},
+			"geometry" : {
+				"x" : 153.09311184910462,
+				"y" : -26.531543248475284
+			}
+		}, {
+			"attributes" : {
+				"Description" : "Yaroomba Beach"
+			},
+			"geometry" : {
+				"x" : 153.10188689463382,
+				"y" : -26.550267215353287
+			}
+		}
+	]
+};
